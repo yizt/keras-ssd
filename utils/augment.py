ab2b1256ea652c29b51ec17f12e84059774a0210
yizt <csuyzt@163.com>
2019-10-26 21:41:38 +0800

    数据增广-flip

--- utils/augment.py
+++ utils/augment.py
@@ -24,11 +24,11 @@ class Resize(object):
         self.out_width = width
         self.interpolation_mode = interpolation_mode
 
-    def __call__(self, image, boxes=None):
+    def __call__(self, image, gt_boxes=None):
         """
 
         :param image: [H,W,3]
-        :param boxes: GT boxes [N,(y1,x1,y2,x2)]
+        :param gt_boxes: GT boxes [N,(y1,x1,y2,x2)]
         :return image:
         :return boxes:
         """
@@ -38,10 +38,67 @@ class Resize(object):
         image = cv2.resize(image,
                            dsize=(self.out_width, self.out_height),
                            interpolation=self.interpolation_mode)
-        if boxes is None:
+        if gt_boxes is None:
             return image
 
-        boxes = boxes.copy()
+        boxes = gt_boxes.copy()
         boxes[:, [0, 2]] = np.round(boxes[:, [0, 2]] * (self.out_height / img_height), decimals=0)
         boxes[:, [1, 3]] = np.round(boxes[:, [1, 3]] * (self.out_width / img_width), decimals=0)
         return image, boxes
+
+
+class Flip:
+    """
+    水平或者垂直翻转图像
+    """
+
+    def __init__(self, dim):
+        """
+
+        :param dim: 翻转维度；horizontal-水平，vertical-垂直
+        """
+        self.dim = dim
+
+    def __call__(self, image, gt_boxes=None):
+        """
+
+        :param image: [H,W,3]
+        :param gt_boxes: GT boxes [N,(y1,x1,y2,x2)]
+        :return:
+        """
+        img_height, img_width = image.shape[:2]
+
+        if self.dim == 'horizontal':  # 水平翻转
+            image = image[:, ::-1]
+            if gt_boxes is None:
+                return image
+
+            boxes = np.copy(gt_boxes)
+            boxes[:, [1, 3]] = img_width - boxes[:, [3, 1]]
+            return image, boxes
+        else:  # 垂直翻转
+            image = image[::-1]
+            if gt_boxes is None:
+                return image
+
+            boxes = np.copy(gt_boxes)
+            boxes[:, [0, 2]] = img_height - boxes[:, [2, 0]]
+            return image, boxes
+
+
+class HorizontalFlip(Flip):
+    """
+    水平翻转
+    """
+
+    def __init__(self):
+        super(HorizontalFlip, self).__init__(dim='horizontal')
+
+
+class VerticalFlip(Flip):
+    """
+    垂直翻转
+    """
+
+    def __init__(self):
+        super(VerticalFlip, self).__init__(dim='vertical')
