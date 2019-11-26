5f9f2ee5f6ed7fce8a36a32cf73b9894fe8596d2
yizt <csuyzt@163.com>
2019-11-26 12:35:39 +0800

    demo

---
+++ demo.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+ @File    : demo.py
+ @Time    : 2019/11/26 上午11:47
+ @Author  : yizuotian
+ @Description    :
+"""
+
+import argparse
+import sys
+import time
+import numpy as np
+from config import cfg
+from utils import np_utils
+from utils.preprocess import PredictionTransform
+from ssd import ssd_model
+import cv2
+
+
+def main(args):
+    from tensorflow.python.keras import backend as K
+    import tensorflow as tf
+
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = tf.Session(config=config)
+    K.set_session(session)
+    # 加载图像
+    im = cv2.imread(args.image_path)
+    h, w = im.shape[:2]
+    trans = PredictionTransform(cfg.image_size, cfg.mean_pixel, cfg.std)
+    image = trans(im[:, :, ::-1])
+
+    # 加载模型
+    m = ssd_model(cfg.feature_fn, cfg.input_shape, cfg.num_classes, cfg.specs,
+                  score_threshold=cfg.score_threshold,
+                  iou_threshold=cfg.iou_threshold,
+                  max_detections_per_class=cfg.max_detections_per_class,
+                  max_total_detections=cfg.max_total_detections,
+                  stage='test')
+    m.load_weights(args.weight_path, by_name=True)
+    # m.summary()
+    # 预测边框、得分、类别
+    s_time = time.time()
+    boxes, scores, class_ids = m.predict(image[np.newaxis, :, :, :])
+    print("预测耗时：{} 秒".format(time.time() - s_time))
+    # 去除padding
+    scores = np_utils.remove_pad(scores[0])[:, 0]
+    class_ids = np_utils.remove_pad(class_ids[0])[:, 0]
+    boxes = np_utils.remove_pad(boxes[0])
+    # 还原检测边框到
+    boxes = np.clip(boxes, 0, cfg.image_size)
+    boxes[:, [0, 2]] *= h / cfg.image_size
+    boxes[:, [1, 3]] *= w / cfg.image_size
+
+    # 画边框
+    for box, score, class_id in zip(boxes.astype(np.int32), scores, class_ids):
+        y1, x1, y2, x2 = box
+        text = '{}:{:03f}'.format(list(cfg.class_mapping.keys()[class_id]))
+        cv2.putText(im, text, (x1 + 8, y2 + 20), cv2.FONT_HERSHEY_PLAIN,
+                    1.5, (0, 255, 0), thickness=1)
+        cv2.rectangle(im, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    cv2.imwrite(args.result_path, im)
+
+
+if __name__ == '__main__':
+    parse = argparse.ArgumentParser()
+    parse.add_argument("--weight_path", type=str, default=None, help="weight path")
+    parse.add_argument("--image_path", type=str, default=None, help="image path")
+    parse.add_argument("--result_path", type=str, default=None, help="output image path")
+    arguments = parse.parse_args(sys.argv[1:])
+    main(arguments)
